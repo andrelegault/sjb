eb7e3a6c58d8a92d4cb5fc9fe989dbe02793c6be
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-24 02:25:32 -0400

    Add user model (#4)

---
+++ lib/models/user.dart
@@ -0,0 +1,24 @@
+/// User model represents a user signing up
+class User {
+  final String firstName;
+  final String lastName;
+  final DateTime dob;
+  final String city;
+  final String studyField;
+  final PositionStatus positionStatus;
+  final String term;
+
+  User(
+      {this.firstName,
+      this.lastName,
+      this.dob,
+      this.city,
+      this.studyField,
+      this.positionStatus,
+      this.term});
+}
+
+enum PositionStatus {
+  FULL_TIME,
+  PART_TIME,
+}
