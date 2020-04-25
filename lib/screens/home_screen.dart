2b76949ce05ab7c9de51590cca0c4a6cd04519e3
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-25 18:26:02 -0400

    Show name using provider (#14)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sjb/models/user.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: Icon(Icons.home)),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text("Hi, User!"),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    RaisedButton(
-                        child: Text("Visit listings"),
-                        onPressed: () =>
-                            Navigator.of(context).pushNamed('/listings'))
-                  ]),
-            ],
-          ),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('Hi, ${Provider.of<User>(context).name}!'),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                      child: Text('Visit listings'),
+                      onPressed: () =>
+                          Navigator.of(context).pushNamed('/listings')),
+                  RaisedButton(
+                      child: Text('Log out'),
+                      onPressed: () =>
+                          Navigator.of(context).pushReplacementNamed('/'))
+                ]),
+          ],
+        )),
       );
 }
