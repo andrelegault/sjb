565ab39010bf49f16b6ffeefa5b8fdcd9895e19f
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-25 20:36:16 -0400

    Add fromJson constructor

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 class User extends ChangeNotifier {
   String email;
   String name;
+  String password;
   DateTime dob;
   String city;
   StudyField studyField;
@@ -23,6 +24,14 @@ class User extends ChangeNotifier {
       this.studyField = StudyField.soen,
       this.positionStatus = PositionStatus.full_time,
       this.term});
+
+  User.fromJson(Map<String, dynamic> data)
+      : email = data['email'],
+        password = data['password'],
+        name = data['name'],
+        dob = data['dob'],
+        studyField = StudyField.values[data['studyField']],
+        positionStatus = PositionStatus.values[data['positionStatus']];
 }
 
 enum StudyField {
