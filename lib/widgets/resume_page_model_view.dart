05afe49a6754176de7aeb31fdd77e34595186227
Andre Parsons-Legault <17802877+andrelegault@users.noreply.github.com>
2020-04-26 01:21:42 -0400

    Remove unused code

--- lib/widgets/resume_page_model_view.dart
+++ lib/widgets/resume_page_model_view.dart
@@ -3,9 +3,7 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:provider/provider.dart';
 import 'package:sjb/models/page_model.dart';
-import 'package:sjb/models/user.dart';
 
 import 'name_page_model_view.dart';
 
